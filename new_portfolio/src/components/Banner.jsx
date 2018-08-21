fb76a663c703b77fa6bb56c55d7b0385ab218b8c
Becky McClain <beckykelley8@yahoo.com>
2018-08-21 15:01:41 -0700

    setting up home page

---
+++ new_portfolio/src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Banner extends Component {
+    render() {
+        return(
+            <div className="banner">
+            <h1>Hi, I'm Becky, 
+                <br />
+                a web developer</h1>
+            </div>
+        )
+    }
+}
+
+export default Banner;
---
+++ new_portfolio/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Banner from './components/Banner'
+import './app.css'
+
+ReactDOM.render(
+    <div>
+        <Banner />
+    </div>, document.getElementById('root')
+)
